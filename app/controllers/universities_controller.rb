470d99aa4573ca1f91dd37b9ccceae829d68be18
ibrahim777764 <ibrahimnaseer896@gmail.com>
2023-04-12 09:39:30 +0500

    add dashboard for univercity

--- app/controllers/universities_controller.rb
+++ app/controllers/universities_controller.rb
@@ -1,19 +1,24 @@
 class UniversitiesController < ApplicationController
-  def index
-    @universities = University.all
-  end
-
   def new
     @university = University.new
   end
 
   def create
     @university = University.new(university_params)
+
     if @university.save
-      flash[:success] = "University created successfully!"
-      redirect_to universities_path
+      session[:university_id] = @university.id # store the university ID in the session
+      redirect_to university_dashboard_path # redirect to the dashboard path
     else
-      render 'new'
+      render :new
+    end
+  end
+
+  def university_dashboard
+    @university = University.find_by(id: session[:university_id]) # retrieve the university from the session
+
+    if @university.nil?
+      redirect_to new_university_path # redirect to the registration page if the university is not found
     end
   end
 
@@ -23,16 +28,3 @@ class UniversitiesController < ApplicationController
     params.require(:university).permit(:name, :logo, :contact_info, :about)
   end
 end
-
-def create
-  @university = University.new(university_params)
-  if params[:university][:logo].present?
-    @university.logo.attach(params[:university][:logo])
-  end
-  if @university.save
-    redirect_to @university
-  else
-    render 'new'
-  end
-end
-
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get '/students', to: 'students#index'
   get 'register/student', to: 'students#new', as: 'register_student'
   get 'register/university', to: 'universities#new', as: 'register_university'
+  get 'university_dashboard', to: 'universities#university_dashboard'
+
 
   resources :universities do
     resources :courses
@@ -10,4 +12,12 @@ Rails.application.routes.draw do
   resources :students, only: [:new, :create]
 
   root to: 'home#index'
+  Rails.application.routes.draw do
+  # ...
+  resources :universities do
+    get 'dashboard', on: :collection
+  end
+  # ...
+end
+
 end
